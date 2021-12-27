a9667668652f0bfd977f24a18a6dcb0d2352ab6e
emorilebo <emorylebo@gmail.com>
2021-12-27 14:23:45 +0100

    ADD menus model

--- lib/model/menus.dart
+++ lib/model/menus.dart
@@ -2,10 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Menus {
   String? menuID;
-  String? sellerID;
+  String? sellerUID;
   String? menuTitle;
   String? menuInfo;
   Timestamp? publishedDate;
   String? thumbnailUrl;
   String? status;
+
+  Menus(
+      {this.menuID,
+      this.sellerUID,
+      this.menuTitle,
+      this.menuInfo,
+      this.publishedDate,
+      this.thumbnailUrl,
+      this.status});
+
+  Menus.fromJson(Map<String, dynamic> json) {
+    menuID = json['menuID'];
+    sellerUID = json['sellerUID'];
+    menuTitle = json['menuTitle'];
+    menuInfo = json['menuInfo'];
+    publishedDate = json['publishedDate'];
+    thumbnailUrl = json['thumbnailUrl'];
+    status = json['status'];
+  }
 }
