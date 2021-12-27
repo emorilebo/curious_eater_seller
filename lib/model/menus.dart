706ab7a755b996ccbdd2edd416dcbde6d3fbcb65
emorilebo <emorylebo@gmail.com>
2021-12-27 12:00:37 +0100

    ADD menus model

---
+++ lib/model/menus.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Menus {
+  String? menuID;
+  String? sellerID;
+  String? menuTitle;
+  String? menuInfo;
+  Timestamp? publishedDate;
+  String? thumbnailUrl;
+  String? status;
+}
